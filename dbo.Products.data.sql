e9302f6e838e656aa517d9f4aa3c96f971284455
wessam <wessam.hassan@gmail.com>
2022-06-01 11:49:47 +0200

    adding imgs and add products script

---
+++ dbo.Products.data.sql
@@ -0,0 +1,85 @@
+﻿INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Scone with jam & cream cheese
+', N'Scones with organic Swedish blackberry jam.
+', N'Scone with jam _ cream cheese.jpg', CAST(45.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Breakfast roll with cheddar
+', N'Breakfast roll with cheddar, butter, green salad & tomato.
+', N'Breakfast roll with cheddar.jpg', CAST(40.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Breakfast roll cheddar & turkey pastrami
+
+', N'Breakfast roll with cheddar, turkey pastrami, butter, green salad & tomato.
+
+', N'Breakfast roll cheddar _ turkey pastrami.jpg', CAST(43.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Americano', N'A long black coffee with 2 shots of espresso for regular, and 3 for grande.', N'Americano.jpg', CAST(35.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Caffe latte', N'Espresso topped with steamed milk and a thin layer of micro-foam.', N'Caffe latte.jpg', CAST(32.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Cappuccino
+', N'Espresso with steamed milk, microfoam, and dusted with chocolate powder.
+', N'Cappuccino.jpg', CAST(35.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Butter Croissant', N'An all butter, flaky French pastry, rolled into a croissant.', N'Butter Croissant.jpg', CAST(32.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Cardamom bun premium', N'Hand-knotted cardamom bun with real butter.', N'Cardamom bun premium.jpg', CAST(34.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( N'Spandau lemoncurd & blueberries', N'Danish pastry with sour and fresh lemoncurd, blueberries and icing sugar.', N'Spandau lemoncurd _ blueberries.jpg', CAST(33.00 AS Decimal(5, 2)))
+
+
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Yoghurt honey walnut',
+ 'Lactose free yoghurt 10% with honey and walnuts.',
+ 'Yoghurt honey walnut.jpg', 
+ CAST(40.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Croissant prosciutto & egg',
+ 'Stuffed premium butter croissant with prosciutto crudo & scrambled egg.',
+ 'Croissant prosciutto _ egg.jpg', 
+ CAST(35.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Salad, Chicken, Turkey bacon, Farro Calabrese & egg',
+ 'Swedish chicken with turkey bacon, pesto calabrese farro & boiled egg.',
+ 'Salad, Chicken, Turkey bacon, Farro Calabrese _ egg.jpg', 
+ CAST(65.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Salad tabboulhe with pomegranate & spicy hummus',
+ 'Housemade tabboulhe with spicy hummus, pomegranate, olive oil & limepepper.',
+ 'Salad tabboulhe with pomegranate _ spicy hummus.jpg', 
+ CAST(62.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Grilled pinsa tonno piccante',
+ 'Italian light bread with pesto, hot tuna in mayonaise, jalapenos, tomatoes. Grilled and heated in the oven.',
+ 'Grilled pinsa tonno piccante.jpg', 
+ CAST(60.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Grilled pinsa pesto prosciutto',
+ 'Italian light bread with pesto, grated mozzarella, fresh tomatoes, prosciutto crudo. Grilled and heated in the oven.',
+ 'Grilled pinsa pesto prosciutto.jpg', 
+ CAST(60.00 AS Decimal(5, 2)))
+
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Caramel Latte',
+ 'Our award-winning espresso, steamed milk, topped with whipped cream and caramel sauce.',
+ 'Caramel Latte.jpg', 
+ CAST(40.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Classic Frappe Latte',
+ 'Classic Frappe Latte on our award winning espresso.',
+ 'Classic Frappe Latte.jpg', 
+ CAST(35.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Ice Chai',
+ 'Classic Iced chai.',
+ 'Ice Chai.jpg', 
+ CAST(40.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Ice Latte Caramel',
+ 'Classic Iced Latte on our award winning espresso and caramel syrup.',
+ 'Ice Latte Caramel.jpg', 
+ CAST(42.00 AS Decimal(5, 2)))
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Ice Latte Vanilla',
+ 'Classic Iced Latte on our award winning espresso and vanilla syrup.',
+ 'Ice Latte Vanilla.jpg', 
+ CAST(42.00 AS Decimal(5, 2)))
+
+
+
+
+INSERT INTO [dbo].[Products] ( [Name], [Description], [ImageUrl], [Price]) VALUES ( 'Blueberry crumble pie with vanilla sauce',
+ 'Classic handmade crumble pie filled with blueberries and served with vanilla sauce.',
+ 'Blueberry crumble pie with vanilla sauce.jpg', 
+ CAST(36.00 AS Decimal(5, 2)))
